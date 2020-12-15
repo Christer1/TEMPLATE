49d4520fe142a67b365dcf71ec068a6759f46764
OLAWALE <yussupholawale2017@gmail.com>
2020-12-15 07:44:48 +0100

    update login

---
+++ Login Template/register.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Register - Naira4Coin</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+
+<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+	<style>
+		body {
+			font-family: 'Courier New', Courier, monospace;
+		}
+	</style>
+</head>
+<body>
+<div class="limiter">
+<div class="container-login100">
+<div class="wrap-login100">
+<div class="login100-pic js-tilt" data-tilt>
+<img src="images/img-01.webp" alt="IMG">
+</div>
+<form class="login100-form validate-form" autocomplete="off">
+<span class="login100-form-title">
+Registration Form
+</span>
+
+<div class="wrap-input100 validate-input" data-validate="Enter your surnanme">
+<input class="input100" type="text" name="surname" placeholder="Surname">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-user" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="Enter your name">
+<input class="input100" type="text" name="name" placeholder="Name">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-user" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="Enter your preferred username">
+<input class="input100" type="text" name="username" placeholder="Username (For login)">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-user" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="Enter a valid email address">
+<input class="input100" type="email" name="email" placeholder="Email">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-envelope" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="Enter your phone number">
+<input class="input100" type="tel" name="phoneNumber" placeholder="Phone Number">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-phone" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="Enter a vaild whatsapp number">
+<input class="input100" type="text" name="surname" placeholder="Valid whatsapp number">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-whatsapp" aria-hidden="true"></i>
+</span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate="Password is required">
+<input class="input100" type="password" name="password" placeholder="Password">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
+</div>    
+<div class="wrap-input100 validate-input" data-validate="Confirm password is required">
+<input class="input100" type="password" name="confirm_password" placeholder="Confirm Password">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock" aria-hidden="true"></i>
+</span>
+</div>
+<div class="container-login100-form-btn">
+<button class="login100-form-btn">Register</button>
+</div>
+<!-- <div class="text-center p-t-12">
+<span class="txt1">
+Forgot
+</span>
+<a class="txt2" href="#">Password?</a>
+</div> -->
+<div class="text-center p-t-136">
+<a class="txt2" href="login.php">
+Login to your account
+<i class="fa fa-arrow-right m-l-5" aria-hidden="true"></i>
+</a>
+</div>
+</form>
+</div>
+</div>
+</div>
+
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="vendor/select2/select2.min.js"></script>
+
+<script src="vendor/tilt/tilt.jquery.min.js"></script>
+<script>
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+
+<script src="js/main.js"></script>
+</body>
+</html>
